48520330852e8f95f6edfb10c967eeca31b32d7c
yandong.yan <yandong.yan@upai.com>
2018-03-28 16:20:19 +0800

    feat: add netconf rest

---
+++ ryu/demo/rest_upwan_switch.py
@@ -0,0 +1,297 @@
+# Copyright (C) 2012 Nippon Telegraph and Telephone Corporation.
+# Copyright (C) 2012 Isaku Yamahata <yamahata at private email ne jp>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+This module provides a set of REST API for switch configuration.
+- Per-switch Key-Value store
+
+Used by OpenStack Ryu agent.
+"""
+
+import logging
+import numbers
+import socket
+import struct
+
+import json
+
+# import netconf_switch
+
+from netconf_switch import NetconfSwitch
+
+from six.moves import http_client
+
+from ryu.app.wsgi import ControllerBase
+from ryu.app.wsgi import Response
+from ryu.app.wsgi import WSGIApplication
+from ryu.base import app_manager
+from ryu.lib import hub
+from ryu.exception import RyuException
+from ryu.controller import conf_switch
+from ryu.lib import dpid as dpid_lib
+
+SWITCHID_PATTERN = dpid_lib.DPID_PATTERN + r'|all'
+
+REST_RESULT = 'result'
+REST_DETAILS = 'details'
+REST_OK = 'success'
+REST_NG = 'failure'
+REST_ALL = 'all'
+REST_SWITCHID = 'switch_id'
+RESR_NAME = 'name'
+REST_USERNAME = 'username'
+REST_PASSWORD = 'password'
+REST_HOST = 'host'
+REST_PORT = 'port'
+
+
+class NotFoundError(RyuException):
+    message = 'switch is not connected. : switch_id=%(switch_id)s'
+
+
+class CommandFailure(RyuException):
+    pass
+
+# REST command template
+def rest_command(func):
+    def _rest_command(*args, **kwargs):
+        try:
+            msg = func(*args, **kwargs)
+            return Response(content_type='application/json',
+                            body=json.dumps(msg))
+
+        except SyntaxError as e:
+            status = 400
+            details = e.msg
+        except (ValueError, NameError) as e:
+            status = 400
+            details = e.message
+
+        except NotFoundError as msg:
+            status = 404
+            details = str(msg)
+
+        msg = {REST_RESULT: REST_NG,
+               REST_DETAILS: details}
+        return Response(status=status, body=json.dumps(msg))
+
+    return _rest_command
+
+class UpWanNetconfClient(NetconfSwitch):
+
+    def __init__(self, name, host, port, username, password):
+        self._name = name
+        super(UpWanNetconfClient, self).__init__(
+            host=host, port=port, username=username, password=password,
+            device_params={"name":"h3c"},
+            unknown_host_cb=lambda host, fingeprint: True)
+    
+    def get_switch(self):
+        pass
+
+    def _do_of_config(self):
+        self._do_get()
+        self._do_get_config('running')
+        self._do_get_config('startup')
+
+        # LINC doesn't support 'candidate' datastore
+        try:
+            self._do_get_config('candidate')
+        except ncclient.NCClientError:
+            traceback.print_exc()
+
+        # use raw XML format
+        self._do_edit_config(SWITCH_PORT_DOWN)
+        self._do_edit_config(SWITCH_ADVERTISED)
+        self._do_edit_config(SWITCH_CONTROLLER)
+
+        self._set_ports_down()
+
+        self.switch.close_session()
+
+
+class UpWanSwitchController(ControllerBase):
+    _SWITCH_LIST = {}
+    _LOGGER = None
+
+    def __init__(self, req, link, data, **config):
+        super(UpWanSwitchController, self).__init__(req, link, data, **config)
+        # self.conf_switch = data
+    @classmethod
+    def set_logger(cls, logger):
+        cls._LOGGER = logger
+        cls._LOGGER.propagate = False
+        hdlr = logging.StreamHandler()
+        fmt_str = '[RT][%(levelname)s] : %(message)s'
+        hdlr.setFormatter(logging.Formatter(fmt_str))
+        cls._LOGGER.addHandler(hdlr)
+
+    @classmethod
+    def register_switch(cls, switch_id, data ):
+        details = None
+        cls._LOGGER.info('register_switch:%s',switch_id)
+
+        try:
+            # Set address data
+            name = data[RESR_NAME]
+            username = data[REST_USERNAME]
+            password = data[REST_PASSWORD]
+            port = data[REST_PORT]
+            host = data[REST_HOST]
+            cls._LOGGER.info('username:%s',username)
+            switch = UpWanNetconfClient(name, host, port, username, password)    
+            details = 'register new neconf switch [switch_id=%s,name=%s]' % (switch_id,name)
+            
+        except CommandFailure as err_msg:
+            msg = {REST_RESULT: REST_NG, REST_DETAILS: str(err_msg)}
+            msg.setdefault(REST_SWITCHID, switch_id)
+            return msg
+
+        if details is not None:
+            msg = {REST_RESULT: REST_OK, REST_DETAILS: details}
+            cls._SWITCH_LIST.setdefault(switch_id, switch)
+
+            cls._LOGGER.info('Join netconf switch. %s', switch_id)
+            msg.setdefault(REST_SWITCHID, switch_id)            
+            return msg
+        else:
+            raise ValueError('Invalid parameter.')                
+        
+        
+    @classmethod
+    def unregister_switch(cls, switch_id):
+        if switch_id in cls._SWITCH_LIST:
+            cls._SWITCH_LIST[switch_id].delete()
+            del cls._SWITCH_LIST[switch_id]
+            cls._LOGGER.info('Leave switch %s.', switch_id)
+
+    @rest_command
+    def list_switches(self, _req, **_kwargs):
+        # dpids = self.conf_switch.dpids()
+        body = json.dumps("hello list_switches")
+        return Response(content_type='application/json', body=body)
+
+    # @rest_command
+    # def get_switch(self, req, switch_id, **_kwargs):
+    #     # dpids = self.conf_switch.dpids()
+    #     body = json.dumps("hello get_switch")
+    #     return Response(content_type='application/json', body=body)
+
+    @rest_command
+    def get_switch(self, req, switch_id, **_kwargs):
+        self._LOGGER.info('Join netconf switch:%s',switch_id)
+        return self._access_switch(switch_id,'get_switch', req)
+
+
+    # @rest_command        
+    # def connect_switch(self, _req, switch_id, **_kwargs):
+    #     self._LOGGER.info('Join netconf switch:%s',switch_id)
+    #     return self._access_switch(switch_id,'config_switch', req)
+
+    @rest_command         
+    def config_switch(self, _req, switch_id, **_kwargs):
+        self._LOGGER.info('Join netconf switch:%s',switch_id)
+        try:
+            param = _req.json if _req.body else {}
+        except ValueError:
+            raise SyntaxError('invalid syntax %s', _req.body)
+
+        msg = UpWanSwitchController.register_switch(switch_id,param)
+        body = json.dumps(msg)
+        return Response(content_type='application/json', body=body)        
+        # return self._access_switch(switch_id,'config_switch', req)
+
+    @rest_command         
+    def delete_switch(self, _req, switch_id, **_kwargs):
+        self._LOGGER.info('Join netconf switch:%s',switch_id)
+        return self._access_switch(switch_id,'delete_switch', req)
+
+    def _access_switch(self, switch_id, func, req):
+        rest_message = []
+        self._LOGGER.info('_access_switch:%s',switch_id)        
+        switches = self._get_switch(switch_id)
+        try:
+            param = req.json if req.body else {}
+        except ValueError:
+            raise SyntaxError('invalid syntax %s', req.body)
+        for switch in switches.values():
+            function = getattr(switch, func)
+            # data = function(param, self.waiters)
+            rest_message.append(data)
+        rest_message.append("hello _access_switch")
+        return rest_message
+
+    def _get_switch(self, switch_id):
+        switches = {}
+        self._LOGGER.info('_get_switch: %s',switch_id)        
+        if switch_id == REST_ALL:
+            switches = self._SWITCH_LIST
+        else:
+            sw_id = dpid_lib.str_to_dpid(switch_id)
+            if sw_id in self._SWITCH_LIST:
+                switches = {sw_id: self._SWITCH_LIST[sw_id]}
+
+        if switches:
+            self._LOGGER.info('find switch: %s',switch_id)        
+            return switches
+        else:
+            raise NotFoundError(switch_id=switch_id)
+
+
+class UpWanSwitchAPI(app_manager.RyuApp):
+    # _CONTEXTS = {
+    #     'conf_switch': conf_switch.ConfSwitchSet,
+    # }
+    _CONTEXTS = {'wsgi': WSGIApplication}
+
+
+    def __init__(self, *args, **kwargs):
+        super(UpWanSwitchAPI, self).__init__(*args, **kwargs)
+        wsgi = kwargs['wsgi']
+        mapper = wsgi.mapper
+
+        # use data
+        self.conf_switch = {}
+
+        UpWanSwitchController.set_logger(self.logger)
+        controller = UpWanSwitchController
+
+        wsgi.registory[controller.__name__] = self.conf_switch
+        requirements = {'switch_id': SWITCHID_PATTERN}
+
+        # For no vlan data
+        rootPath = '/upwan'
+        path = '/upwan/{switch_id}'
+        mapper.connect('upwan', rootPath, controller=UpWanSwitchController,
+                    #    requirements=requirements,
+                       action='list_switches',
+                       conditions=dict(method=['GET']))
+        mapper.connect('upwan', path, controller=UpWanSwitchController,
+                       requirements=requirements,
+                       action='get_switch',
+                       conditions=dict(method=['GET']))                       
+        # mapper.connect('upwan', rootPath, controller=UpWanSwitchController,
+        #             #    requirements=requirements,
+        #                action='connect_switch',
+        #                conditions=dict(method=['POST']))
+        mapper.connect('upwan', path, controller=UpWanSwitchController,
+                       requirements=requirements,
+                       action='config_switch',
+                       conditions=dict(method=['POST']))                       
+        mapper.connect('upwan', path, controller=UpWanSwitchController,
+                       requirements=requirements,
+                       action='delete_switch',
+                       conditions=dict(method=['DELETE']))
